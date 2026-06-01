7d2cc93b5d59f179aeecd70cd465a5ccd1b7edc4
mxs65616 <mxs65616@gmail.com>
2026-05-31 17:43:59 -0700

    Features/course-section
    add two new cards to course curriculum section

--- client/src/pages/sections/BonusSection.tsx
+++ client/src/pages/sections/BonusSection.tsx
@@ -24,6 +24,16 @@ export const BonusSection = (): JSX.Element => {
       description:
         "Give students an idea of how they can expect to grow throughout your course. Include specificity and precise results so students can benchmark exactly what they'll learn.",
     },
+    {
+      title: "Behavioral & Leadership Stories: Craft compelling narratives that resonate",
+      description:
+        "Give students an idea of how they can expect to grow throughout your course. Include specificity and precise results so students can benchmark exactly what they'll learn.",
+    },
+    {
+      title: "Offer Negotiation & Career Strategy: Land the role and maximize your package",
+      description:
+        "Give students an idea of how they can expect to grow throughout your course. Include specificity and precise results so students can benchmark exactly what they'll learn.",
+    },
   ];
 
   return (
